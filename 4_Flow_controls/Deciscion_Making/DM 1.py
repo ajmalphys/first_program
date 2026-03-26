0a7baf00e5ec1e737ebde1a41b32614c6965efb4
ajmalphys <ajmalphysix@gmail.com>
2026-03-26 15:46:21 +0530

    flow controls 26.03.2026

---
+++ 4_Flow_controls/Deciscion_Making/DM 1.py
@@ -0,0 +1,1 @@
+#descions are made based on a condition
---
+++ 4_Flow_controls/flow_control.py
@@ -0,0 +1,6 @@
+
+'''Topics'''
+#1... decision making / conditional statements
+#2... Looping
+#3... Jumping Statements
+#---------------------------
